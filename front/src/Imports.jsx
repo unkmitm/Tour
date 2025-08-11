c10ea5de182e243e3d70b2882a77f30d2b72cf85
Ehsan <unkmitm@gmail.com>
2025-08-11 23:12:31 +0330

    case sensitivity in import paths

--- front/src/Imports.jsx
+++ front/src/Imports.jsx
@@ -13,7 +13,7 @@ import MountainTours from "./components/tours/MountainTours";
 import NorthTours from "./components/tours/NorthTours";
 import CampingTours from "./components/tours/CampingTours";
 import Footer from "./Footer";
-import SignUp from "./components/common/pages/SignUp";
+import SignUp from "./components/common/pages/SignUp.jsx";
 import SearchBox from "./search/searchbox.jsx";
 import RecentSearches from "./search/recentsearches.jsx";
 import Login from "./components/common/pages/Login";
